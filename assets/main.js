12728a55498669d8115590565a7bc1b4907b47e1
Cesar Arias <Cearias3010@gmail.com>
2023-02-28 03:13:26 -0500

    Updates

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+const API='https://youtube-v31.p.rapidapi.com/search?channelId=UC7bX_RrH3zbdp5V4j5umGgw&part=snippet%2Cid&order=date&maxResults=9';const content=null||document.getElementById('content');const options={method:'GET',headers:{'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+'X-RapidAPI-Key': '1a0b4b193emsh4adeb31d1f53cebp1075cbjsnb815068cf60b'}};async function fetchData(urlApi){const response=await fetch(urlApi,options);const data=await response.json();return data;}
+(async()=>{try{const videos=await fetchData(API);let view=`
+    ${videos.items.map(video=>`
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `).slice(0,4).join('')}
+    `;content.innerHTML=view;}catch(error){console.log(error);}})();
